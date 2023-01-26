87ffdd2b0af507240bcea30b76ece0022ecec564
anderson92zolis <azolis92@hotmail.es>
2023-01-27 00:28:50 +0100

    N1E2 HECHO

---
+++ src/n1exercici2/TreballadorPresencial.java
@@ -0,0 +1,49 @@
+package n1exercici2;
+
+public class TreballadorPresencial extends Treballador{
+	
+	private static int benzina=100;
+
+	public TreballadorPresencial(String nom, String cognom, int preuHora) {
+		super(nom, cognom, preuHora);
+		// TODO Auto-generated constructor stub
+	}
+
+	// Getters&Setters
+	
+	/**
+	 * @return the benzina
+	 */
+	public static int getBenzina() {
+		return benzina;
+	}
+
+	/**
+	 * @param benzina the benzina to set
+	 */
+	public static void setBenzina(int benzina) {
+		TreballadorPresencial.benzina = benzina;
+	}
+
+	// methods
+
+	@Override
+	public int metodeCalcularSou(int horesTreballades) {
+		// TODO Auto-generated method stub
+		return super.metodeCalcularSou(horesTreballades)+benzina;
+	}
+	
+	@Deprecated
+	public void obsoleteMethodTreballadorPresencial() {
+		System.out.println("javadoc tag is an ad hoc annotation indicating that the method should no longer be used. ");;
+		}
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
+
+	
+	
+		
+}
